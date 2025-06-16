a354dd99d59378c65f70fcf169ece4b561b4f455
njahn88 <nja010@email.latech.edu>
2025-06-16 11:58:30 -0500

    Started working on Dust 2 blog post

--- src/pages/BlogPages/Dust2Analysis.jsx
+++ src/pages/BlogPages/Dust2Analysis.jsx
@@ -1,7 +1,17 @@
 export default function Dust2Analysis(){
     return (
         <>
-            Dust 2 Page
+            <div className={'flex justify-center font-outfit font-bold py-3 text-2xl'}>
+                Level Design Analysis: Counter Strike 2
+            </div>
+            <div className={'flex justify-start px-100 font-outfit'}>
+                Dust 2, a map which has become synonymous with the Counter Strike franchise, was released in 2001.
+                Since it's release over two decades ago the map has seen a handful of changes to adhere to games
+                current design choices. While the visuals of the map have changed to match the current games look,
+                the design has largely stayed the same. I will be looking at Dust 2 in it's current form in Counter Strike
+                2 and how the design choices it was created under have shaped it into one of the most famous maps in video game
+                history.
+            </div>
         </>
     )
 }
