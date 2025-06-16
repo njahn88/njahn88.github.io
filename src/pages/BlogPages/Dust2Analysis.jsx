52e784a4fea4d41d3f304bcb19db12164ff991ed
njahn88 <nja010@email.latech.edu>
2025-06-16 14:48:37 -0500

    Finished the gameplay design portion of the map

--- src/pages/BlogPages/Dust2Analysis.jsx
+++ src/pages/BlogPages/Dust2Analysis.jsx
@@ -13,6 +13,12 @@ import BSiteOverview from '/src/img/Dust2Analysis/BSite-Overview.png'
 import BSiteMap from '/src/img/Dust2Analysis/BSite-Map.png'
 import BSiteSite from '/src/img/Dust2Analysis/BSite-Site.png'
 import BSiteSite2 from '/src/img/Dust2Analysis/BSite-Site2.png'
+import BSiteBackSite from '/src/img/Dust2Analysis/BSite-Backsite.png'
+import BSiteCar1 from '/src/img/Dust2Analysis/BSite-Car1.png'
+import BSiteCar2 from '/src/img/Dust2Analysis/BSite-Car2.png'
+import Middle1 from '/src/img/Dust2Analysis/Middle-1.png'
+import Middle2 from '/src/img/Dust2Analysis/Middle-2.png'
+import Middle3 from '/src/img/Dust2Analysis/Middle-3.png'
 
 export default function Dust2Analysis(){
     return (
@@ -50,7 +56,7 @@ export default function Dust2Analysis(){
                     </li>
                 </ul>
             </div>
-            <div className={'flex justify-start px-100 font-outfit font-bold text-3xl pt-10'}>
+            <div className={'flex justify-start px-100 font-outfit font-bold text-4xl pt-10'}>
                 Gameplay Design
             </div>
             <div className={'flex justify-start px-100 font-outfit'}>
@@ -177,7 +183,81 @@ export default function Dust2Analysis(){
             <div className={'flex justify-start px-100 font-outfit pt-5'}>
                 This position has the most vision to cover ratio of all the positions on the sight, but while it strives in these
                 areas it lacks in repositioning ability. The player depending on where exactly they are positioned on site will give
-                them vision of "tunnels", "window", and "doors". 
+                them vision of "tunnels", "window", and "doors", but if there is a lot of pressure coming from the main entrance onto
+                the site, "tunnels", then the player will have a lot of trouble getting out from that position and will likely have to
+                "go down with the ship". The designers of this site kept in mind the speed at which the attacks will come which is very quick.
+                Due to this speed, the positions on the sight need to have a little bit more stability when compared to the positions on
+                the A bomb site.
+            </div>
+            <div className={'flex justify-start px-100 font-outfit pt-5'}>
+                The next position I would like to look at is the "Back Plat" position. This position is very similar to the "Goose"
+                position back on A site in that the player is given the most amount of vision to the "tunnels", but has very little
+                repositioning ability.
+            </div>
+            <div className={'flex justify-center pt-10'}>
+                <img src={BSiteBackSite} alt={'Level Design Analysis'} />
+            </div>
+            <div className={'flex justify-start px-100 font-outfit pt-5'}>
+                The designers wanted to make sure that there was a position that would give the most vision to the most
+                important entry point into the site, but also making sure that positon would not be too strong by severely
+                limiting the repositioning ability of anyone who chooses to defend from it.
+            </div>
+            <div className={'flex justify-start px-100 font-outfit pt-5'}>
+                The last position I want to look at on the B bomb site would be the "Car" position which serves an almost
+                identical role to the "Car" position on the A site which the most amount of vision with the least amount
+                of repositioning ability.
+            </div>
+            <div className={'flex justify-center pt-10'}>
+                <div className={'grid grid-cols-2 w-300 gap-10'}>
+                    <img src={BSiteCar1} alt={'A Site Cover 1'} className={'w-200 h-100'}/>
+                    <img src={BSiteCar2} alt={'Level Design Analysis'} className={'h-100'}/>
+                </div>
+            </div>
+            <div className={'flex justify-start px-100 font-outfit pt-5'}>
+                This position allows the player to see the "doors" and "window" entrances to the sight while also
+                being able to see any player who enter through the "tunnels". To counter balance the vision this
+                position is given is to reduce the movement of the player who will be stuck in the corner and
+                limiting the amount of cover given to the player.
+            </div>
+            <div className={'flex justify-start px-100 font-outfit pt-5'}>
+                The developers of the B bomb site had a few key points they wanted to balance being the speed at which
+                attackers will be able to get onto the sight and the short range nature of the site. Going into the design
+                process they had to add a suitable amount of cover for players to use due to the size of the map as a
+                whole and knowing that there would likely only be 2-3 players there at any given time. As a result you have
+                a bomb site that encourages quick attacking action and for the defenders, ample cover.
+            </div>
+            <div className={'flex justify-start px-100 font-outfit font-bold text-3xl pt-10'}>
+                Middle
+            </div>
+            <div className={'flex justify-start px-100 font-outfit pt-5'}>
+                The first position that is not on a bomb sight. The developers wanted to give this position the most
+                amount of vision possible at the cost of very little cover and three access points.
+            </div>
+            <div className={'flex justify-center pt-10'}>
+                <div className={'grid grid-row-1 w-300 gap-10'}>
+                    <img src={Middle1} alt={'A Site Cover 1'} className={'h-100 row-start-1'}/>
+                    <img src={Middle2} alt={'Level Design Analysis'} className={'h-100 row-start-1'}/>
+                    <img src={Middle3} alt={'Level Design Analysis'} className={'h-100 row-start-1'}/>
+                </div>
+            </div>
+            <div className={'flex justify-start px-100 font-outfit pt-5'}>
+                The middle position of Dust 2 is primarily held for players with longer range weapons who want to
+                be able to get information or help defend the A or B bomb site. The developers knew that this would
+                be too strong of a position if they gave the player too much direct vision and so to counter this they
+                added two sets of doors, one for middle and the other for the B bomb site. These doors prevent the player
+                from getting full vision of either area unless they reposition, giving up sight of other areas. What is
+                position balances is the amount of direct vision of bomb sites, the amount of cover, and the repositioning
+                abilities. It leans heavily into the repositioning ability of the player, limits the direct vision given
+                to the player, and heavily reduces the amount of cover.
+            </div>
+            <div className={'flex justify-start px-100 font-outfit pt-5 font-bold'}>
+                While is analysis mainly covers the Counter Terrorist perspective of the map, it shows how the
+                developers had to think in order to create a healthy, fun, and replayable map. Balancing the amount of
+                vision, repositioning ability, and cover to give the Counter Terrorists a fair amount of power when
+                defending.
+            </div>
+            <div className={'flex justify-start px-100 font-outfit font-bold text-4xl pt-10'}>
+                Visual Clarity
             </div>
         </>
     )
