8c4950f8c73f3263b956430e9b7487e92dff83dd
njahn88 <nja010@email.latech.edu>
2025-06-16 15:46:42 -0500

    files for upload

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -4,7 +4,7 @@ export default function Blog(){
     return(
         <>
             <div className={'flex justify-center items-center flex-wrap'}>
-                <BlogPost title={'Level Design Analysis: Counter Strike 2'}  description={'In depth look into the level design decisions of the Counter Strike 2 map.'}/>
+                <BlogPost title={'Level Design Analysis: Counter Strike 2'}  description={'In depth look into the level design decisions of the Counter Strike 2 map and what went into creating one of the most iconic maps in video game history.' }/>
                 <hr className={'border-gray-300 mx-6 w-full'} />
             </div>
         </>
