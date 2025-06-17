f59d25f74c54283f4a9c5ef91778dfc736f90b68
njahn88 <nja010@email.latech.edu>
2025-06-17 13:56:26 -0500

    Added to new player experience

--- src/pages/BlogPages/ReworkLolTutorial.jsx
+++ src/pages/BlogPages/ReworkLolTutorial.jsx
@@ -52,6 +52,58 @@ export default function ReworkLolTutorial() {
                 to finish the tutorials at all. This is why I think there needs to be full
                 overhaul of the tutorial system and some added UI changes to try and address these issues.
             </div>
+            <div className={'flex justify-center font-outfit font-bold py-3 text-4xl text-center items-center'}>
+                <hr className={'border-gray-300 mx-6 w-full'} />
+                Rework
+                <hr className={'border-gray-300 mx-6 w-full'} />
+            </div>
+            <div className={'flex justify-start px-10 md:px-100 font-outfit pt-3'}>
+                One of the most fun parts of League of Legends when I first started playing was seeing all the
+                different champions picking one based off of looks and maybe a little look at the abilities. I
+                think this the fist step Riot needs to take before getting into the tutorials, get the player
+                excited to play as a certain champion even if they don't understand anything else.
+            </div>
+            <div className={'flex justify-start px-10 md:px-100 font-outfit font-bold text-3xl pt-10'}>
+                Letting players choose
+            </div>
+            <div className={'flex justify-start px-10 md:px-100 font-outfit pt-3'}>
+                Before any of the tutorials start I believe Riot should let a new player pick any
+                champion they want for free. Allow the player to look through the entire catalogue
+                of champions and pick one that looks interesting to them. The reason for doing this
+                is to allow players have a choice early on to how they will learn the game. This
+                change will effect how the players learn the game and can give a more focused approach
+                to starting the game.
+            </div>
+            <div className={'flex justify-start px-10 md:px-100 font-outfit font-bold text-3xl pt-10'}>
+                Role Based Tutorials
+            </div>
+            <div className={'flex justify-start px-10 md:px-100 font-outfit pt-3'}>
+                This would come right after the player makes their choice of champion and the ensuing
+                tutorial would be tailored to which every champion they picked. For example if the player
+                chose Ashe as their first champion, they would be put into a Bot Lane focused tutorial.
+                This tutorial would cover the basics of player movement and attacking while also teaching
+                the basics of bot lane lane phase. Everything from the dynamic of ADC and Support, when and why
+                to recall, and what to look out for in the jungle. Another example would be if the player chose
+                Elise as their first champion, a jungle focused tutorial would be the only thing the player would
+                be able to select. It would show them where jungle monsters are, pathing suggestions, objectives,
+                ganking, etc. Simply focusing the tutorials to a fit where their champion succeeds the most will
+                lead to players having a better understanding of what they actually need to be doing when playing
+                a real game.
+            </div>
+            <div className={'flex justify-start px-10 md:px-100 font-outfit font-bold text-2xl pt-10'}>
+                Example
+            </div>
+            <div className={'flex justify-start px-10 md:px-100 font-outfit pt-3'}>
+                We can stick with Ashe for this example and see what a focused bot lane tutorial might
+                look like and how it would be better than what is currently available.
+            </div>
+            <div className={'flex justify-start px-10 md:px-125 font-outfit font-bold text-2xl pt-10'}>
+                Start
+            </div>
+            <div className={'flex justify-start px-10 md:px-125 font-outfit pt-3'}>
+                We can stick with Ashe for this example and see what a focused bot lane tutorial might
+                look like and how it would be better than what is currently available.
+            </div>
         </>
     )
 }
