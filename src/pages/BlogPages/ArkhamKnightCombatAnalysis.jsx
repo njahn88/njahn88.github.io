99fc3f44e03919d03680e8580f513d8f2de1d964
njahn88 <nja010@email.latech.edu>
2025-06-18 14:45:59 -0500

    Added starter paragraph for Arkham Knight Analysis

--- src/pages/BlogPages/ArkhamKnightCombatAnalysis.jsx
+++ src/pages/BlogPages/ArkhamKnightCombatAnalysis.jsx
@@ -4,6 +4,13 @@ export default function ArkhamKnightCombatAnalysis() {
             <div className={'flex justify-center font-outfit font-bold py-3 text-2xl text-center'}>
                 Combat Design Analysis: Batman Arkham Knight
             </div>
+            <div className={'flex justify-start px-10 md:px-100 font-outfit'}>
+                Building on the backs of the games that came before it, the Rocksteady combat designers had to ensure the
+                "heart" of the game was still in tact while also adding some new mechanics along the way. What resulted was
+                a heavily polished system that consistently pushed the player to learn and adapt to the enemies actions. This is
+                why I would like to look into what changes the developers made and the techniques they used to create a combat
+                system that anyone could succeed with while also leaving room for skill expression.
+            </div>
         </>
     )
 }
