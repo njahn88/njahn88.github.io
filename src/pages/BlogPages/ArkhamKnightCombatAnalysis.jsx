d28b65dbd1285e2f95b450582ad82c9b3415ec87
njahn88 <nja010@email.latech.edu>
2025-06-20 22:35:07 -0500

    Added first two paragraphs

--- src/pages/BlogPages/ArkhamKnightCombatAnalysis.jsx
+++ src/pages/BlogPages/ArkhamKnightCombatAnalysis.jsx
@@ -14,7 +14,21 @@ export default function ArkhamKnightCombatAnalysis() {
                 how and why they should keep playing. What the developers choose to include in these
                 few minutes must be carefully crafted as to avoid players losing interest or becoming
                 too frustrated to continue playing, so it's easy to say that intros are important.
-                
+                It is this reason that I would like to analyze the intro of Batman Arkham Knight to
+                see where it gets things correct and what could have been changed to help better prepare
+                new players on becoming the "Batman".
+            </div>
+            <div className={'flex justify-start px-10 md:px-100 font-outfit font-bold text-4xl pt-10'}>
+                What purpose does the intro to a video game serve?
+            </div>
+            <div className={'flex justify-start px-10 md:px-100 font-outfit'}>
+                An intro to any good video game should be able to get the player
+                immersed in the world, familiar with a few characters, and most importantly
+                teach the player the basic controls they will need know, all without having
+                to spell it out for them. It's no easy task, but when done correctly the player
+                no longer feels like they are playing a video game. They feel like they are apart
+                of the world, that they are the main character, and that their actions really have
+                an impact.
             </div>
         </>
     )
