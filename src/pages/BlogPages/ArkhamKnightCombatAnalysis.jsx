18a702d14974edf40911adfadd7e360f63344559
njahn88 <nja010@email.latech.edu>
2025-06-20 22:22:57 -0500

    Rephrasing batman blog

--- src/pages/BlogPages/ArkhamKnightCombatAnalysis.jsx
+++ src/pages/BlogPages/ArkhamKnightCombatAnalysis.jsx
@@ -10,7 +10,11 @@ export default function ArkhamKnightCombatAnalysis() {
                 Arkham Knight Intro Design Analysis
             </div>
             <div className={'flex justify-start px-10 md:px-100 font-outfit'}>
-                The intro of any video game is the most important half an hour
+                The intro of a video game. Half an hour of precisely tuned gameplay to teach players
+                how and why they should keep playing. What the developers choose to include in these
+                few minutes must be carefully crafted as to avoid players losing interest or becoming
+                too frustrated to continue playing, so it's easy to say that intros are important.
+                
             </div>
         </>
     )
