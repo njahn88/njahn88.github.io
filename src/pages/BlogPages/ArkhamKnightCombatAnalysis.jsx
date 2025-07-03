3f73f7dd6d4f625dff39bc88f0bcfc3f37dfa0b9
njahn88 <nja010@email.latech.edu>
2025-07-03 16:59:39 -0500

    Rewording arkham knight blog

--- src/pages/BlogPages/ArkhamKnightCombatAnalysis.jsx
+++ src/pages/BlogPages/ArkhamKnightCombatAnalysis.jsx
@@ -10,7 +10,7 @@ export default function ArkhamKnightCombatAnalysis() {
                 Arkham Knight Intro Design Analysis
             </div>
             <div className={'flex justify-start px-10 md:px-100 font-outfit'}>
-                Imagine. You are Magnus Carlson, a world renown chess player, getting ready to start
+                You are Magnus Carlson, a world renown chess player, getting ready to start
                 a tournament. For the first match of the tournament you are paired against an opponent with
                 20 years less experience than you. You sit down at the chess board, shake hands, and the match
                 starts, but for some reason you can't remember what the pieces do anymore. The opponent makes their
@@ -22,11 +22,34 @@ export default function ArkhamKnightCombatAnalysis() {
             <div className={'flex justify-start px-10 md:px-100 py-5 font-outfit'}>
                 Just after the intro cutscene and brief stint of gameplay, you are Batman, a crime fighting master, someone who
                 has put countless villans away time after time. You drop into a pack of grunts and for some reason you can't remember
-                how to punch, counter, or dodge. You turn to mashing the controller hoping that something will work. For most players
-                that do this you will succeed. The developers made it so it was very, very difficult to lose this fight and for good reason.
-                The problem is that for a lot of players who might not have played any Arkham games before this doesn't help them FEEL like
-                Batman and is why I will be giving my thoughts on what Rocksteady could have done to teach the player the core mechanics of the
-                game before throwing them into the fire.
+                how to punch, counter, or dodge. Most players who are new to the series will likely resort to mashing their controller, hoping
+                they hit the right button at the right time. This lack of true understanding could lead to frustration to a point where they might
+                not want to continue playing a game where they are supposed to FEEL like Batman. This is why I will be giving my thoughts on what
+                Rocksteady could have done to teach the player the core mechanics of the game before throwing them into the fire.
+            </div>
+            <div className={'flex justify-start px-10 md:px-100 font-outfit font-bold text-4xl pt-10'}>
+                What makes a great intro?
+            </div>
+            <div className={'flex justify-start px-10 md:px-100 py-5 font-outfit'}>
+                The intro of any video game might just be the most important half an hour a developer has to make a good impression
+                on the player. It's not easy, but if done well can make the transition from "tutorial" to actual gameplay nearly
+                imperceptible. This is what you want. No text boxes that take up the entire screen explaining everything an enemy
+                is about to do. No sections of games that are actually marked "Tutorial". All of this can lead players to feel like they
+                are in a class...
+            </div>
+            <div className={'flex justify-start px-10 md:px-100 py-2 font-outfit font-bold italic'}>
+                Teacher - "Hey class you will need to memorize these paragraphs for an upcoming test!"
+            </div>
+            <div className={'flex justify-start px-10 md:px-100 py-2 font-outfit font-bold italic'}>
+                Student - "When is the test?"
+            </div>
+            <div className={'flex justify-start px-10 md:px-100 py-2 font-outfit font-bold italic'}>
+                Teacher - "Now!"
+            </div>
+            <div className={'flex justify-start px-10 md:px-100 py-5 font-outfit'}>
+                It doesn't make any sense and you would fail. Games shouldn't do this. Games are a form of entertainment, not a sit down
+                lecture. With all that said it doesn't mean that games can't have text or clearly laid out "Tutorials" it's just that
+                there are better and more seamless ways of giving the player the same information.
             </div>
         </>
     )
